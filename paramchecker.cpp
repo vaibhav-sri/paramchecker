b14f6acc8ade86db7b2a347f0dd44ff1142d557c
Sudeep <sudeep.rp@gmail.com>
2020-07-29 18:15:08 +0530

    c++ trial

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -1,12 +1,17 @@
+#include "paramchecker.h"
+#include <vector>
 
-bool vitalsAreOk(float bpm, float spo2, float respRate) {
-  int i;
-  if(bpm < 70 || bpm > 150) {
-    return false;
-  } else if(spo2 < 80) {
-    return false;
-  } else if(respRate < 30 || respRate > 60) {
-    return false;
+IVitalCheck* vitalCheckers[] = {
+  [bpm] = new VitalRangeCheck(70, 150),
+  [spo2] = new VitalRangeCheck(80, 100),
+  [respRate] = new VitalRangeCheck(30, 60),
+  [avgECG] = new VitalValueCheck(0),
+};
+
+std::vector<bool> vitalsAreOk(const std::vector<Measurement>& measurements) {
+  std::vector<bool> results;
+  for(auto t = measurements.begin(); t != measurements.end(); t++) {
+    results.push_back(vitalCheckers[t->id]->measurementIsOk(t->measured_value));
   }
-  return true;
+  return results;
 }
--- paramchecker.h
+++ paramchecker.h
@@ -1,2 +1,47 @@
+#include <vector>
+#include <cstdlib>
 
-bool vitalsAreOk(float bpm, float spo2, float respRate);
+enum VITAL_ID {
+  bpm,
+  spo2,
+  respRate,
+  avgECG
+};
+
+struct Measurement{
+    VITAL_ID id;
+    float measured_value;
+};
+
+class IVitalCheck {
+  public:
+    virtual bool measurementIsOk(float measurement) = 0; //pure virtual
+};
+
+class VitalRangeCheck: public IVitalCheck {
+  public:
+    explicit VitalRangeCheck(float lower, float upper) {
+      m_lower = lower;
+      m_upper = upper;
+    }
+    virtual bool measurementIsOk(float measurement) {
+      return (measurement >= m_lower && measurement <= m_upper);
+    }
+  private:
+    float m_lower;
+    float m_upper;
+};
+
+class VitalValueCheck: public IVitalCheck {
+  public:
+    explicit VitalValueCheck(float alarmValue) {
+      m_alarmValue = alarmValue;
+    }
+    virtual bool measurementIsOk(float measurement) {
+      return std::abs(measurement - m_alarmValue) < 0.001;
+    }
+  private:
+    float m_alarmValue;
+};
+
+std::vector<bool> vitalsAreOk(const std::vector<Measurement>& measurements);
